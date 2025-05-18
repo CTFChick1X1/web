cbf90cf9d7190bd013e787a9c403a503c764928d
CTFChick1X1 <mwangiessy2007@gmail.com>
2025-05-18 17:47:24 +0300

    .

--- coreapp/models.py
+++ coreapp/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 
 class PostBlog(models.Model):
-    name = CharField(max_length=50, blank=False)
+    title = models.CharField(max_length=50, blank=False)
+    content = models.TextField(blank=False, )
