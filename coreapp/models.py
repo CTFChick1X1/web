594607a5d07f7bafbef50aecefd7078b3a7d9666
C9b3rD3vi1 <nicksonwe@gmail.com>
2025-05-18 18:05:20 +0300

    blog classification

--- coreapp/models.py
+++ coreapp/models.py
@@ -1,17 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class PostBlog(models.Model):
-    title = models.CharField(max_length=50, blank=False)
-    image = models.ImageField(upload_to='images/', blank=False)
-    content = models.TextField(blank=False, null=False)
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class BlogPost(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
-
-    class Meta:
-        ordering = ['-created_at']
+    published = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(BlogPost, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Comment by {self.name}"
