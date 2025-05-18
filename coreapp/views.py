1c142692e152a49342ec7fcc6a015a63d721f6ed
C9b3rD3vi1 <nicksonwe@gmail.com>
2025-05-18 19:12:19 +0300

    ..

--- coreapp/views.py
+++ coreapp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
+    portfolio = Project.objects.all()
+    blogpost = BlogPost.objects.all().order_by('-created_at')[:3]
+
     return render(request, 'home.html')
+
 
 def about(request):
     return render(request, 'about.html')
