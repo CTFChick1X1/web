48242ccffbeac54f85403dc77777dbec364b91d2
C9b3rD3vi1 <nicksonwe@gmail.com>
2025-05-18 17:55:59 +0300

    post blog model code

--- coreapp/models.py
+++ coreapp/models.py
@@ -4,4 +4,14 @@ from django.db import models
 
 class PostBlog(models.Model):
     title = models.CharField(max_length=50, blank=False)
-    content = models.TextField(blank=False, )
+    image = models.ImageField(upload_to='images/', blank=False)
+    content = models.TextField(blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
